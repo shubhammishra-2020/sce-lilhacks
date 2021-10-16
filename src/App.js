1af81970fc153f8c4fac6be90995cd5a44844289
shubhammishra-2020 <shubhammishra@sjsu.edu>
2021-10-15 20:22:34 -0700

    web

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-
 function App() {
+  function getData(val){
+    console.warn(val.target.value)
+  }
   return (
-    <div className="App">
+    <div class = "Navi GO">
+      <center>
+      <h1>Welcome To Navi GO</h1>
+      <h2>The one stop getaway</h2>
+      </center>
+
+      <center> <p>Activity <input id = "text1" placeholder=" " onChange = {getData} /> </p>
+      <p>Enter your getaway <input id="text1"  placeholder=" " onChange = {getData}/> </p>
+      <p class = "input-boxes">When <input id="text1" placeholder = " " onChange = {getData} /> </p>
+      <button>Enter</button>
+      </center>
+
+      
     </div>
   );
 }
 
 export default App;
+
+
+     
