c7a4117a092b2ccd92f2af67a01688470125c5a7
shubhammishra-2020 <shubhammishra@sjsu.edu>
2021-10-18 21:56:51 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,130 @@
 import './App.css';
-import React, {useState} from 'react';
-import GoogleMap from './components/GoogleMap';
+import logo2 from './logo2.png';
+import { formatRelative } from "date-fns";
+import PlacesAutocomplete from 'react-places-autocomplete';
+import React from 'react';
+import LocationMarker from './components/LocationMarker'
 
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 
-function App() {
-  function getData(val){
-    console.warn(val.target.value)
-  }
-  const [selected, setSelected] = useState("Choose One");
-  return (
-   
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+
+import '@reach/combobox/styles.css';
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+const center = {
+  lat: 37.334665328,
+  lng: -121.875329832,
+
+};
+const options = {
+
   
-    <div class = "App-header">
-      
-      <center>
-      <h1>Welcome To Navi GO</h1>
-      
-      </center>
-      <center> 
-      
-      <p>Enter your getaway <input id="text1"  placeholder=" " onChange = {getData}/> </p>
-      <p class = "input-boxes">When <input id="text1" placeholder = " " onChange = {getData} /> </p>
-      <button>Enter</button>
-      <GoogleMap />
-      </center>
-      
+};
 
-      
-    </div>
-  );
-}
-
-export default App;
+// export default function App() {
+//   const { isLoaded, loadError } = useLoadScript({
+//     googleMapsApiKey: 'AIzaSyBdy4-SVNXM_tlxzDzZu6DFtwiPmFJ54-E',
+//     libraries,
+//   });
+ 
 
 
-     
+//   if (loadError) return "Error";
+//   if (!isLoaded) return "Loading...";
+
+//   return <div>
+
+//     <Search/>
+//     <GoogleMap mapContainerStyle = {mapContainerStyle}
+//     zoom = {15} 
+//     center = {center}
+//     options = {options}
+//     style={{height: '100px', width: '100px'} }
+//     >
+    
+//     </GoogleMap>
+//   </div>;
+// }
+// function Search() {
+//   const{ready, value, suggestions: {status, data}, setValue, clearSuggestion, } = usePlacesAutocomplete({
+
+//     requestOptions: {
+//       location: {
+// lat: () => 37.334665328,
+// lng: () => -121.875329832 }, 
+// radius: 200 * 1000,
+
+//     },
+//   });
+//   return (
+//     <div className="search">
+//       <Combobox onSelect={(address) => {
+//         console.log(address);
+//       }}>
+//         <ComboboxInput
+//           value={value}
+//           onChange={(e) => {setValue(e.target.value);
+//           }}
+          
+//           placeholder="Search your location"
+//         />
+//         <ComboboxPopover>
+          
+//             {status === "OK" &&
+//               data.map(({ id, description }) => (
+//                 <ComboboxOption key={id} value={description}/>
+//               ))}
+          
+//         </ComboboxPopover>
+//       </Combobox>
+      
+//     </div>
+  
+//   );
+
+    
+//   }
+export default function App() {
+  
+   return (
+    
+     <div class = "App-header">
+       return <img  src={logo2} alt="logo"/>  
+       <center>
+       <h1>Welcome To Navi GO</h1>
+       </center>
+      
+       <center>
+       <br />
+       <p>Enter your getaway <input id="text1"  placeholder=" "/> </p>
+       <p class = "input-boxes">When <input id="text1" placeholder = " "/> </p>
+      
+       <button>Enter</button>
+       <br />
+       <GoogleMap/>
+       </center>
+     </div>
+
+   );
+ }
+//export default App
--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+<script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBdy4-SVNXM_tlxzDzZu6DFtwiPmFJ54-E&libraries=places&callback=initMap">
+</script>
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
